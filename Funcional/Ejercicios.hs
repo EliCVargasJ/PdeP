a1ed97dc7cababaa14339adef90717438a0be71e
EliCVargasJ <vargasjelizabethc@gmail.com>
2025-04-29 19:20:26 -0300

    Algunos ejercicios realizados, orden de carpetas.

--- Funcional/Ejercicios.hs
+++ Funcional/Ejercicios.hs
@@ -1,4 +1,4 @@
-import Text.Show.Functions
+
 
 
 -- 1. Definir la función esMultiploDeTres/1, que devuelve True si un número es múltiplo de 3
@@ -19,3 +19,18 @@ cubo n= n*n*n
 
 -- 4. Definir la función area/2, devuelve el área de un rectángulo a partir de su base y su altura.
 
+-- Inicialmente defini area como area :: Int -> Int -> Float  pero no puedo esperar un 'float' si le mando dos valores del tipo Int 
+
+area :: Float -> Float -> Float  
+
+area base altura  = base * altura
+
+-- 5. Definir la funcion esBisiesto, indica si un anio es bisiesto. (un anio es bisiesto si es divisible por 400 
+--    pero no es divisible por 100 )
+-- Nota: resolverlo utilizando la funcion esMultiploDe
+
+esBisiesto :: Int -> Bool
+esBisiesto anio = esMultiploDe 400 anio || segundaOpcionAnioBisiesto anio
+
+segundaOpcionAnioBisiesto :: Int -> Bool
+segundaOpcionAnioBisiesto anio = not(esMultiploDe 100 anio ) && esMultiploDe 4 anio 
